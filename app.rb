b4eaec30748d436f2de4c6f4b7252e625cd8bb0c
Adam Ladachowski <adam@saiden.pl>
2020-04-22 20:48:12 +0200

    wip

--- app.rb
+++ app.rb
@@ -41,17 +41,58 @@ class Ded
   def self.all
     Util.data_loader(:deds).map { |a| Ded.new(a) }
   end
+
+  def boss
+    Boss.all.select { |b| b.boss_of == @id }.first
+  end
+end
+
+class Boss
+  attr_accessor :id, :boss_of, :loot, :name
+
+  def initialize(boss)
+    @name = boss.name
+    @id = boss.id
+    @boss_of = boss.boss_of
+    @loot = boss.loot
+  end
+
+  def self.all
+    Util.data_loader(:bosses).map { |a| Boss.new(a) }
+  end
+end
+
+class Loot
+  attr_accessor :id, :price, :name
+
+  def initialize(id, name, price)
+    @id = id
+    @name = name
+    @price = price
+  end
+
+  def self.from(ded)
+    types = ESI::UniverseApi.new.post_universe_names ded.boss.loot
+    types.map do |type|
+      price = get_price(type.id)
+      new type.id, type.name, price
+    end
+  end
+
+  def self.prices
+    @prices ||= ESI::MarketApi.new.get_markets_prices
+  end
+
+  def self.get_price(type_id)
+    prices.select { |price| price.type_id == type_id }.first.average_price
+  rescue
+    0
+  end
 end
 
 binding.pry
 
-anoms = data_loader(:anoms)
-bosses = data_loader(:bosses)
-deds = data_loader(:deds)
-
 get '/' do
-  @deds = deds
-  @anoms = anoms
-  @bosses = bosses
+  @anoms = Anom.all
   slim :index
 end
